757e77c0333eb58142ea4021408f32803e8890b4
Xuanhao Song <1834831158@qq.com>
2025-08-25 03:35:13 +0200

    update droid slam

--- H1_low_level_isaac_sim/src/2_plan/mpc/scripts/srbm_scripts/srbm_solver/__init__.py
+++
@@ -1,8 +0,0 @@
-from .solve_traj import solve_traj
-from .solver import setup_acados_ocp_solver, setup_acados_sim_solver
-
-__all__ = [
-    "setup_acados_ocp_solver",
-    "setup_acados_sim_solver",
-    "solve_traj",
-]
